794d46e6130af4ff226b3a45c88c45c3d2b73430
Ashley Allen <mail@ashallendesign.co.uk>
2020-12-14 21:06:17 +0000

    Initial commit.

---
+++ src/Exceptions/MailboxLayerException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AshAllenDesign\MailboxLayer\Exceptions;
+
+use Exception;
+
+/**
+ * Class MailboxLayerException.
+ *
+ * A generic exception that can be used within the
+ * package.
+ */
+class MailboxLayerException extends Exception
+{
+}
---
+++ src/Providers/MailboxLayerProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AshAllenDesign\MailboxLayer\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MailboxLayerProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/mailbox-layer.php', 'mailbox-layer');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     * @throws ValidationException
+     */
+    public function boot(): void
+    {
+        // Config
+        $this->publishes([
+            __DIR__.'/../../config/mailbox-layer.php' => config_path('mailbox-layer.php'),
+        ], 'mailbox-layer-config');
+    }
+}
