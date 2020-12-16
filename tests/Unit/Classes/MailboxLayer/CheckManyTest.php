d7c94e7872a75cc238076cac9864a0bfbb934292
Ashley Allen <mail@ashallendesign.co.uk>
2020-12-16 19:03:21 +0000

    Updated tests.

--- tests/Unit/Classes/MailboxLayer/CheckManyTest.php
+++ tests/Unit/Classes/MailboxLayer/CheckManyTest.php
@@ -4,6 +4,7 @@ namespace AshAllenDesign\MailboxLayer\Tests\Unit\Classes\MailboxLayer;
 
 use AshAllenDesign\MailboxLayer\Classes\MailboxLayer;
 use AshAllenDesign\MailboxLayer\Tests\Unit\TestCase;
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
@@ -13,6 +14,8 @@ class CheckManyTest extends TestCase
     /** @test */
     public function many_emails_can_be_validated_via_the_api()
     {
+        Carbon::setTestNow(now());
+
         Cache::shouldReceive('get')
             ->once()
             ->withArgs(['mailboxlayer_result_mail@ashallendesign.co.uk'])
@@ -24,7 +27,7 @@ class CheckManyTest extends TestCase
             ->andReturnNull();
 
         Http::fake([
-            'https://apilayer.net/api/check?access_key=123&email=mail%40ashallendesign.co.uk&smtp=1' => Http::response([
+            'https://apilayer.net/api/check?access_key=123&email=mail%40ashallendesign.co.uk&smtp=1'     => Http::response([
                 'email'       => 'mail@ashallendesign.co.uk',
                 'didYouMean'  => '',
                 'user'        => 'mail',
@@ -69,6 +72,7 @@ class CheckManyTest extends TestCase
         $this->assertEquals(false, $result[0]->disposable);
         $this->assertEquals(false, $result[0]->free);
         $this->assertEquals(0.8, $result[0]->score);
+        $this->assertEquals(now(), $result[0]->validatedAt);
 
         $this->assertEquals('support1@ashallendesign.co.uk', $result[1]->email);
         $this->assertEquals('support@ashallendesign.co.uk', $result[1]->didYouMean);
@@ -80,6 +84,7 @@ class CheckManyTest extends TestCase
         $this->assertEquals(true, $result[1]->disposable);
         $this->assertEquals(true, $result[1]->free);
         $this->assertEquals(0.7, $result[1]->score);
+        $this->assertEquals(now(), $result[1]->validatedAt);
     }
 
     /** @test */
@@ -90,18 +95,19 @@ class CheckManyTest extends TestCase
             ->once()
             ->withArgs(['mailboxlayer_result_mail@ashallendesign.co.uk'])
             ->andReturn([
-                'email'       => 'mail@ashallendesign.co.uk',
-                'didYouMean'  => '',
-                'user'        => 'mail',
-                'domain'      => 'ashallendesign.co.uk',
-                'formatValid' => true,
-                'mxFound'     => true,
-                'smtpCheck'   => true,
-                'catchAll'    => false,
-                'role'        => true,
-                'disposable'  => false,
-                'free'        => false,
-                'score'       => 0.8,
+                'email'        => 'mail@ashallendesign.co.uk',
+                'didYouMean'   => '',
+                'user'         => 'mail',
+                'domain'       => 'ashallendesign.co.uk',
+                'formatValid'  => true,
+                'mxFound'      => true,
+                'smtpCheck'    => true,
+                'catchAll'     => false,
+                'role'         => true,
+                'disposable'   => false,
+                'free'         => false,
+                'score'        => 0.8,
+                'validatedAt' => now()->subDays(5)->startOfDay()
             ]);
 
         // Set a cached value that we can get.
@@ -109,18 +115,19 @@ class CheckManyTest extends TestCase
             ->once()
             ->withArgs(['mailboxlayer_result_support1@ashallendesign.co.uk'])
             ->andReturn([
-                'email'       => 'support1@ashallendesign.co.uk',
-                'didYouMean'  => 'support@ashallendesign.co.uk',
-                'user'        => 'support1',
-                'domain'      => 'ashallendesign.co.uk',
-                'formatValid' => false,
-                'mxFound'     => false,
-                'smtpCheck'   => false,
-                'catchAll'    => true,
-                'role'        => false,
-                'disposable'  => true,
-                'free'        => true,
-                'score'       => 0.7,
+                'email'        => 'support1@ashallendesign.co.uk',
+                'didYouMean'   => 'support@ashallendesign.co.uk',
+                'user'         => 'support1',
+                'domain'       => 'ashallendesign.co.uk',
+                'formatValid'  => false,
+                'mxFound'      => false,
+                'smtpCheck'    => false,
+                'catchAll'     => true,
+                'role'         => false,
+                'disposable'   => true,
+                'free'         => true,
+                'score'        => 0.7,
+                'validatedAt' => now()->subYear()->startOfDay()
             ]);
 
         // Assert that the HTTP client is never called.
@@ -141,6 +148,7 @@ class CheckManyTest extends TestCase
         $this->assertEquals(false, $result[0]->disposable);
         $this->assertEquals(false, $result[0]->free);
         $this->assertEquals(0.8, $result[0]->score);
+        $this->assertEquals(now()->subDays(5)->startOfDay(), $result[0]->validatedAt);
 
         $this->assertEquals('support1@ashallendesign.co.uk', $result[1]->email);
         $this->assertEquals('support@ashallendesign.co.uk', $result[1]->didYouMean);
@@ -152,5 +160,6 @@ class CheckManyTest extends TestCase
         $this->assertEquals(true, $result[1]->disposable);
         $this->assertEquals(true, $result[1]->free);
         $this->assertEquals(0.7, $result[1]->score);
+        $this->assertEquals(now()->subYear()->startOfDay(), $result[1]->validatedAt);
     }
 }
--- tests/Unit/Classes/ValidationResult/MakeFromResponseTest.php
+++ tests/Unit/Classes/ValidationResult/MakeFromResponseTest.php
@@ -4,12 +4,49 @@ namespace AshAllenDesign\MailboxLayer\Tests\Unit\Classes\ValidationResult;
 
 use AshAllenDesign\MailboxLayer\Classes\ValidationResult;
 use AshAllenDesign\MailboxLayer\Tests\Unit\TestCase;
+use Carbon\Carbon;
 
 class MakeFromResponseTest extends TestCase
 {
     /** @test */
-    public function new_object_is_returned_with_correct_fields_set()
+    public function new_object_is_returned_with_correct_fields_set_and_the_validatedAt_date_is_already_set()
     {
+        Carbon::setTestNow(now());
+
+        $responseData = [
+            'email'        => 'mai1l@ashallendesign.co.uk',
+            'did_you_mean' => 'mail@ashallendesign.co.uk',
+            'user'         => 'mai1l',
+            'domain'       => 'ashallendesign.co.uk',
+            'format_valid' => true,
+            'smtp_check'   => true,
+            'role'         => true,
+            'disposable'   => false,
+            'free'         => false,
+            'score'        => 0.8,
+            'validated_at' => now()
+        ];
+
+        $newObject = ValidationResult::makeFromResponse($responseData);
+
+        $this->assertEquals('mai1l@ashallendesign.co.uk', $newObject->email);
+        $this->assertEquals('mail@ashallendesign.co.uk', $newObject->didYouMean);
+        $this->assertEquals('mai1l', $newObject->user);
+        $this->assertEquals('ashallendesign.co.uk', $newObject->domain);
+        $this->assertEquals(true, $newObject->formatValid);
+        $this->assertEquals(true, $newObject->smtpCheck);
+        $this->assertEquals(true, $newObject->role);
+        $this->assertEquals(false, $newObject->disposable);
+        $this->assertEquals(false, $newObject->free);
+        $this->assertEquals(0.8, $newObject->score);
+        $this->assertEquals(now(), $newObject->validatedAt);
+    }
+
+    /** @test */
+    public function new_object_is_returned_with_correct_fields_set_and_the_validatedAt_date_is_not_already_set()
+    {
+        Carbon::setTestNow(now());
+
         $responseData = [
             'email'        => 'mai1l@ashallendesign.co.uk',
             'did_you_mean' => 'mail@ashallendesign.co.uk',
@@ -35,5 +72,7 @@ class MakeFromResponseTest extends TestCase
         $this->assertEquals(false, $newObject->disposable);
         $this->assertEquals(false, $newObject->free);
         $this->assertEquals(0.8, $newObject->score);
+        $this->assertEquals(now(), $newObject->validatedAt);
     }
+
 }
